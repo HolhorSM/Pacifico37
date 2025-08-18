59ec0eacbec0ad2625239c403300735948f5a289
YoshioSM <chuuck883@gmail.com>
2025-08-18 14:16:34 -0600

    Update
    Balanse de rutas y mejoras en site pasifico

--- site/src/App.jsx
+++ site/src/App.jsx
@@ -1,11 +1,19 @@
 import React from "react"
 import { Route, Routes } from "react-router-dom"
+import Navbar from "./Components/Navbar"
+import Footer from "./Components/Footer"
+import Home from "./Pages/Home"
 
 function App() {
   return (
    <div>
+      <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
 
-
+        
+      <Footer />
    </div>
   )
 }
